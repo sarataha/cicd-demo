e0dfbb79e621204976d931b88d2fa066dabdca04
Sara Taha <st.elzayat@gmail.com>
2018-03-26 11:48:16 -0400

    Update deployment script

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -3,4 +3,6 @@ docker login --email $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 
 docker build -f Dockerfile -t sarataha/cicd-demo:latest .
 
-docker push sarataha/cicd-demo:latest
+docker tag cicd-demo sarataha/cicd-demo
+
+docker push sarataha/cicd-demo
