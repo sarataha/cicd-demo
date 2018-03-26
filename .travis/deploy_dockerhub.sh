5cfd192d14b447a493aac7f42704d77bd68981fd
Sara Taha <st.elzayat@gmail.com>
2018-03-26 11:42:29 -0400

    Update docker image

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-	TAG="latest"
-else
-	TAG="$TRAVIS_BRANCH"
-fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG
+docker login --email $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+
+docker build -f Dockerfile -t sarataha/cicd-demo:latest .
+
+docker push sarataha/cicd-demo:latest
